607a8e1279aa805656adc58dbd9a9cc1fd91d7aa
Saphal Kafle <saphalkafle77@gmail.com>
2026-07-25 19:51:21 +0545

    loading .env

--- groq_client.py
+++ groq_client.py
@@ -1,11 +1,13 @@
 import os
 from groq import Groq
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def get_groq_client():
 
     #get client from api
-    api_key = os.environ.get("GROQ_API_KEY")
+    api_key = os.getenv("GROQ_API_KEY")
 
     if not api_key:
         raise ValueError("Groq_api_Key is not set")
