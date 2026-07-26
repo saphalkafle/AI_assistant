08afb70592b3cd6c5f662d7f675a2bd8f53197a1
Saphal Kafle <saphalkafle77@gmail.com>
2026-07-26 18:18:23 +0545

    logic

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from groq_client import get_groq_client
+
 client = get_groq_client()
 
 class PersonalAssistant:
     def __init__(self):
         print("Hi, I am your AI assistant ... How can i help you?")
 
-    def ans_query(questions):
+    def ans_query(self):
+
+        question = input("Ask me anything:")
         chat_completion = client.chat.completions.create(
             model = "llama-3.3-70b-versatile",
             messages = [
@@ -15,10 +18,10 @@ class PersonalAssistant:
                 },
                 {
                     "role":"user",
-                    "content":questions
+                    "content":question
                 }
             ],
             temperature = 0.7,
-            max_output_tokens = 1024
+            max_tokens = 1024
         )
-        return chat_completion.choices[0].content.strip()
+        print(chat_completion.choices[0].message.content.strip())
---
+++ test.py
@@ -0,0 +1,5 @@
+from main import PersonalAssistant
+assistant = PersonalAssistant()
+assistant.ans_query()
+
+
